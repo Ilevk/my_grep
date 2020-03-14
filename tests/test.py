2fe01f3c0468f0cf248f434f655b779184f7b234
Ilevk <kcs93023@gmail.com>
2020-03-14 21:22:53 +0900

    Add test.py
    add test.py for template

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestOptions(unittest.TestCase):
+
+    def test_option_F(self):
+        pass
+
+    def test_option_G(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
