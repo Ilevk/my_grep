68ce29c90d27bd1160803ab59895cdb9b6d1c786
Ilevk <kcs93023@gmail.com>
2020-03-14 22:15:05 +0900

    Add Method load_data(), and Test

--- src/my_grep.py
+++ src/my_grep.py
@@ -1,16 +1,26 @@
-class my_grep(object):
+import logging
 
-    def __init__(self, option, pattern, file):
+
+class My_grep(object):
+    def __init__(self, option='', pattern='', file=''):
+        self.context = ''
         self.option = option.upper().split()  # option list
         self.pattern = pattern  # pattern str
         self.file = file  # file name str
 
     def load_data(self):
-        
-        pass
+        logging.info(f'Load Text File: {self.file}')
 
-    def find_pattern(self):
-        pass
+        try:
+            logging.info('Open Text File')
+            with open(self.file) as f:
+                logging.info('Read Text File')
+                self.context = f.read()
+                logging.info('Success Read Text File')
+
+        except FileNotFoundError as Error:
+            logging.info(f'Raise {Error}')
+            raise FileNotFoundError
 
     def print_pattern(self):
         print(self.find_pattern())
--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,22 @@
+import os
+from os.path import join
+
 import unittest
+from src.my_grep import *
 
 
 class TestOptions(unittest.TestCase):
-
     def test_option_F(self):
         pass
 
     def test_option_G(self):
         pass
+
+
+class TestLoadData(unittest.TestCase):
+    def testInvalidFile(self):
+        mg = My_grep(file=join(join(os.getcwd(), join('..', 'data'), 'Invalid.txt')))
+        self.assertRaises(FileNotFoundError, mg.load_data)
 
 
 if __name__ == '__main__':
