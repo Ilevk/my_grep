bae2692d474471cd35fd70955c25d5da480a5c16
Ilevk <kcs93023@gmail.com>
2020-03-15 17:56:48 +0900

    Add Test for setting option

--- tests/test.py
+++ tests/test.py
@@ -6,6 +6,30 @@ from src.my_grep import *
 
 
 class TestOptions(unittest.TestCase):
+    def test_set_option_FG(self):
+        mg = My_grep(option='FG',
+                     pattern='test_pattern',
+                     file=join(join(os.getcwd(), join('..', 'data'), 'Lorem_Ipsum.txt')))
+        self.assertEqual(mg.isRegEx, True)
+
+    def test_set_option_GF(self):
+        mg = My_grep(option='GF',
+                     pattern='test_pattern',
+                     file=join(join(os.getcwd(), join('..', 'data'), 'Lorem_Ipsum.txt')))
+        self.assertEqual(mg.isRegEx, False)
+
+    def test_set_option_G(self):
+        mg = My_grep(option='G',
+                     pattern='test_pattern',
+                     file=join(join(os.getcwd(), join('..', 'data'), 'Lorem_Ipsum.txt')))
+        self.assertEqual(mg.isRegEx, True)
+
+    def test_set_option_F(self):
+        mg = My_grep(option='F',
+                     pattern='test_pattern',
+                     file=join(join(os.getcwd(), join('..', 'data'), 'Lorem_Ipsum.txt')))
+        self.assertEqual(mg.isRegEx, False)
+
     def test_option_F(self):
         pass
 
