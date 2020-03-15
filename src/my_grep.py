df316a2bbe6897dce333d8c75385d7ac233dd7d3
Ilevk <kcs93023@gmail.com>
2020-03-15 17:57:13 +0900

    Add Method set_option at my_grep.py

--- src/my_grep.py
+++ src/my_grep.py
@@ -1,14 +1,22 @@
+import re
 import logging
 
 
 class My_grep(object):
     def __init__(self, option='', pattern='', file=''):
         self.context = ''
-        self.option = option.upper().split()  # option list
+        self.option = option # option list
         self.pattern = pattern  # pattern str
         self.file = file  # file name str
+        self.isRegEx = None
+
+        self.set_option()
 
     def load_data(self):
+        """
+        Load Text file and Read Text data,
+        Assign Texts to self.context variable
+        """
         logging.info(f'Load Text File: {self.file}')
 
         try:
@@ -21,6 +29,30 @@ class My_grep(object):
         except FileNotFoundError as Error:
             logging.info(f'Raise {Error}')
             raise FileNotFoundError
+
+    def set_option(self):
+        """
+        Option,
+        F: Handle pattern as Plain Text
+        G: Handle pattern as Basic RegEx
+
+        Behind option has a priority
+        """
+        # set option F, G
+        if 'F' in self.option and 'G' in self.option :
+            idx_F = self.option.index('F')
+            idx_G = self.option.index('G')
+        elif 'F' in self.option:
+            idx_F = 1
+            idx_G = 0
+        else:
+            idx_F = 0
+            idx_G = 1
+
+        self.isRegEx = True if idx_G > idx_F else False
+
+    def find_pattern(self):
+        pass
 
     def print_pattern(self):
         print(self.find_pattern())
