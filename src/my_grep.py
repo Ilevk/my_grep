e5049b9a71d0b65c22349b3f311374588d278e7a
Ilevk <kcs93023@gmail.com>
2020-03-16 20:41:18 +0900

    Edit my_grep.py
    Add getter, reset method, comments

--- src/my_grep.py
+++ src/my_grep.py
@@ -4,14 +4,15 @@ import logging
 
 class My_grep(object):
     def __init__(self, option='', pattern='', file=''):
-        self.context = None
-        self.option = option # option list
-        self.pattern = pattern  # pattern str
-        self.file = file  # file name str
+        self._context = None
+        self._option = option
+        self._pattern = pattern
+        self._file = file
 
-        self.isRegEx = None
-        self.found_pattern = None
-        self.isFind = False
+        self._isRegEx = None
+
+        self._found_pattern = None
+        self._isFind = False
 
         self.set_option()
 
@@ -20,13 +21,13 @@ class My_grep(object):
         Load Text file and Read Text data,
         Assign Texts to self.context variable
         """
-        logging.info(f'Load Text File: {self.file}')
+        logging.info(f'Load Text File: {self._file}')
 
         try:
             logging.info('Open Text File')
-            with open(self.file) as f:
+            with open(self._file) as f:
                 logging.info('Read Text File')
-                self.context = f.read().split('\n')
+                self._context = f.read().split('\n')
                 logging.info('Success Read Text File')
 
         except FileNotFoundError as Error:
@@ -42,10 +43,10 @@ class My_grep(object):
         Behind option has a priority
         """
         # set option F, G
-        if 'F' in self.option and 'G' in self.option :
-            idx_F = self.option.index('F')
-            idx_G = self.option.index('G')
-        elif 'F' in self.option:
+        if 'F' in self._option and 'G' in self._option:
+            idx_F = self._option.index('F')
+            idx_G = self._option.index('G')
+        elif 'F' in self._option:
             idx_F = 1
             idx_G = 0
         else:
@@ -55,36 +56,101 @@ class My_grep(object):
         self.isRegEx = True if idx_G > idx_F else False
 
     def find_pattern(self):
+        """
+        Find pattern with context
+
+        :return: Boolean, whether find or not find
+        """
         find_list = list()
-        self.isFind = True
+        self._isFind = True
 
         if self.isRegEx:
-            p = re.compile(self.pattern)
-            for i, text in enumerate(self.context):
-                m = p.search('r'+text)
+            logging.info('Using pattern as Regular Expression')
+            p = re.compile(self._pattern)
+            for i, text in enumerate(self._context):
+                m = p.search('r' + text)
                 if m is not None:
                     logging.info(f'{m.group()} is matched')
-                    find_list.append([[i+1, m.start(), m.end()], text.replace('\n', '')])
+                    find_list.append([[i + 1, m.start(), m.end()], text.replace('\n', '')])
         else:
-            p_len = len(self.pattern)
-            for i, text in enumerate(self.context):
-                start_idx = text.find(self.pattern)
+            logging.info('Using pattern as Plain Text')
+            p_len = len(self.p_attern)
+            for i, text in enumerate(self._context):
+                start_idx = text.find(self._pattern)
                 if start_idx != -1:
-                    logging.info(f'[Line {i+1}]{self.pattern} is matched')
-                    find_list.append([[i+1, start_idx, start_idx+p_len], text.replace('\n', '')])
+                    logging.info(f'[Line {i + 1}]{self._pattern} is matched')
+                    find_list.append([[i + 1, start_idx, start_idx + p_len], text.replace('\n', '')])
 
         if len(find_list) < 1:
+            logging.info('No matched text is here.')
             return False
 
-        self.found_pattern = find_list
+        self._found_pattern = find_list
+        logging.info('Matched Text assign')
         return True
 
     def print_pattern(self):
-        if self.found_pattern is not None:
-            for (i, text) in self.found_pattern:
-                print_text = ''.join((text[:i[1]-1], "\033[31m", text[i[1]-1:i[2]-1], "\033[0m", text[i[2]-1: ]))
+        """
+        Print pattern
+        """
+        if self._found_pattern is not None:
+            for (i, text) in self._found_pattern:
+                print_text = ''.join((text[:i[1] - 1], "\033[31m", text[i[1] - 1:i[2] - 1], "\033[0m", text[i[2] - 1:]))
                 print(f'[Line {i[0]}]: {print_text}')
-        elif self.isFind:
-            print('Not Found Any texts matched with pattern')
+        elif self._isFind:
+            logging.info('Not Found Any texts matched with pattern')
         else:
-            print('Please Run find pattern')
+            logging.info('Please Run find pattern')
+
+    def reset(self, option=None, pattern=None, file=None):
+        """
+        Reset My Grep
+        :param option:  str
+        :param pattern: str
+        :param file:    str
+        """
+        logging.info('Reset My grep')
+
+        if option is not None:
+            self._option = option
+            self.set_option()
+            self._isRegEx = None
+        if pattern is not None:
+            logging.info(f'Set new Pattern: {pattern}')
+            self._pattern = pattern
+            logging.info('Initialize variables related to patterns')
+        if file is not None:
+            logging.info(f'Set new file: {pattern}')
+            self._file = file
+            self.load_data()
+
+        self._found_pattern = None
+        self._isFind = False
+
+    @property
+    def context(self):
+        return self._context
+
+    @property
+    def option(self):
+        return self._option
+
+    @property
+    def pattern(self):
+        return self._pattern
+
+    @property
+    def file(self):
+        return self._file
+
+    @property
+    def isRegEx(self):
+        return self._isRegEx
+
+    @property
+    def found_pattern(self):
+        if self._found_pattern is not None:
+            return self.found_pattern
+        else:
+            logging.info('Please Run find pattern')
+            return None
